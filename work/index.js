13d5eccdda8a50cc50216273d0268e7ef5bcab99
linewei <linewei@gmail.com>
2019-08-23 18:21:34 +0800

    update

--- work/index.js
+++ work/index.js
@@ -10,8 +10,6 @@ var app = http.createServer(function(req, res) {
   fileServer.serve(req, res);
 }).listen(8080);
 
-console.log("Listening 8080.");
-
 var io = socketIO.listen(app);
 io.sockets.on('connection', function(socket) {
 
@@ -20,13 +18,12 @@ io.sockets.on('connection', function(socket) {
     var array = ['Message from server:'];
     array.push.apply(array, arguments);
     socket.emit('log', array);
-	console.log(...array);
   }
 
-  socket.on('message', function(message,fromId,toId) {
-    log('Client ID ' + socket.id + ' said: ', message);
+  socket.on('message', function(message) {
+    log('Client said: ', message);
     // for a real app, would be room-only (not broadcast)
-	  socket.broadcast.emit('message', message,fromId,toId);
+    socket.broadcast.emit('message', message);
   });
 
   socket.on('create or join', function(room) {
@@ -40,20 +37,15 @@ io.sockets.on('connection', function(socket) {
       socket.join(room);
       log('Client ID ' + socket.id + ' created room ' + room);
       socket.emit('created', room, socket.id);
-
     } else if (numClients === 1) {
       log('Client ID ' + socket.id + ' joined room ' + room);
-      io.sockets.in(room).emit('join', room);
+      // io.sockets.in(room).emit('join', room);
       socket.join(room);
       socket.emit('joined', room, socket.id);
-      io.sockets.in(room).emit('ready');
+      io.sockets.in(room).emit('ready', room);
+      socket.broadcast.emit('ready', room);
     } else { // max two clients
-//      socket.emit('full', room);
-      log('Client ID ' + socket.id + ' joined room ' + room);
-      io.sockets.in(room).emit('join', room);
-      socket.join(room);
-      socket.emit('joined', room, socket.id);
-      io.sockets.in(room).emit('ready');
+      socket.emit('full', room);
     }
   });
 
@@ -68,8 +60,12 @@ io.sockets.on('connection', function(socket) {
     }
   });
 
-  socket.on('bye', function(){
-    console.log('received bye');
+  socket.on('disconnect', function(reason) {
+    console.log(`Peer or server disconnected. Reason: ${reason}.`);
+    socket.broadcast.emit('bye');
   });
 
+  socket.on('bye', function(room) {
+    console.log(`Peer said bye on room ${room}.`);
+  });
 });
